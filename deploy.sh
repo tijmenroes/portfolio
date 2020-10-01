d88649cf5349d64ef351df49d5b91db3f800ab29
unknown <=>
2020-10-01 12:06:57 +0200

    deployment V2

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+#git init
+git add -A
+git commit -m 'deployment 2'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:tijmenroes/tijmenroes.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+ git push -f git@github.com:tijmenroes/portfolio.git master:gh-pages
+
+cd -
