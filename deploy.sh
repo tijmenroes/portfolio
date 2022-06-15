ba72ad1b14e4f56c811de56d54f16588daf83b38
tijmenroes <=>
2022-06-15 20:23:49 +0200

    deploymentx

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploymentx'
 # git push -f git@github.com:tijmenroes/tijmenroes.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:tijmenroes/portfolio.git master:gh-pages
+ git push -f git@github.com:tijmenroes/portfolio.git develop:gh-pages
 
 cd -
